d25ac7a876aa0f666c7b8b70c26e94218f0ca567
Mark Ryk <marco.look@outlook.com>
2021-01-17 09:37:10 -0300

    Add files via upload

--- Ex015 (Verificador de idade)/script.js
+++ Ex015 (Verificador de idade)/script.js
@@ -1,60 +1,45 @@
-function verificar() {
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var h = window.document.getElementById('hora')
     var data = new Date()
-    var ano = data.getFullYear()
-    var fAno = document.getElementById('txtAno')
-    var res = document.querySelector('div#res')
+    var hora = data.getHours()
+    //var hr = data.getTime()
+    //var hora = 0 //Para testar outras horas manualmente
 
-    if (fAno.value.length == 0 || fAno.value > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente')
-    } else {
-        var fSex = document.getElementsByName('radSex')
-        var idade = ano - fAno.value
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fSex[0].checked) {
-            genero = 'Homem'
-            if (fAno.value == 1991) img.setAttribute('src', 'img/foto_marcos.png')
-            else {
-                if (idade >= 0 && idade < 3) {
-                    //Bebê
-                    img.setAttribute('src', 'img/foto_bebe_m.png')
-                } else if (idade < 10) {
-                    //Criança
-                    img.setAttribute('src', 'img/foto_crianca_m.png')
-                } else if (idade < 21) {
-                    //Adolescente
-                    img.setAttribute('src', 'img/foto_adolescente_m.png')
-                } else if (idade < 50 && idade != 30) {
-                    //Adulto
-                    img.setAttribute('src', 'img/foto_adulto_m.png')
-                } else {
-                    //Idoso
-                    img.setAttribute('src', 'img/foto_idoso_m.png')
-                }
-            }
-        }
-        else {
-            genero = 'Mulher'
-            if (idade >= 0 && idade < 3) {
-                //Bebê
-                img.setAttribute('src', 'img/foto_bebe_f.png')
-            } else if (idade < 10) {
-                //Criança
-                img.setAttribute('src', 'img/foto_crianca_f.png')
-            } else if (idade < 21) {
-                //Adolescente
-                img.setAttribute('src', 'img/foto_adolescente_f.png')
-            } else if (idade < 50) {
-                //Adulto
-                img.setAttribute('src', 'img/foto_adulto_f.png')
-            } else {
-                //Idoso
-                img.setAttribute('src', 'img/foto_idoso_f.png')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        res.appendChild(img)
-    }
+    if (hora >= 0 && hora < 4) {
+        img.src = 'img/img_madrugada.png'
+        document.body.style.background = '#101116'
+        saudacao.innerHTML = 'Boa madrugada'
+    } else if (hora >= 4 && hora < 6) {
+        img.src = 'img/img_madrugada_manha.png'
+        document.body.style.background = '#225C6F'
+        saudacao.innerHTML = 'Bom dia'
+    } else if (hora >= 6 && hora < 7) {
+        img.src = 'img/img_inicio_manha.png'
+        document.body.style.background = '#CEC8AE'
+        saudacao.innerHTML = 'Bom dia'
+    } else if (hora >= 7 && hora < 11) {
+        img.src = 'img/img_final_manha.png'
+        document.body.style.background = '#CCC1BB'
+        saudacao.innerHTML = 'Bom dia'
+    } else if (hora >= 11 && hora < 16) {
+        img.src = 'img/img_tarde.png'
+        document.body.style.background = '#FCBB66'
+        saudacao.innerHTML = 'Boa tarde'
+    } else if (hora >= 16 && hora < 18) {
+        img.src = 'img/img_tarde_16.png'
+        document.body.style.background = '#845565'
+        saudacao.innerHTML = 'Boa tarde'
+    } else if (hora >= 18 && hora < 19) {
+        img.src = 'img/img_fim_de_tarde.png'
+        document.body.style.background = '#524269'
+        saudacao.innerHTML = 'Boa noite'
+    } else if (hora >= 19 && hora <= 23) {
+        img.src = 'img/img_noite.png'
+        document.body.style.background = '#2A4755'
+        saudacao.innerHTML = 'Boa noite'
+    } else window.alert('Hora inválida')
+    msg.innerHTML = `Agora são ${hora} horas.`
+    h.innerHTML = `${data}`
 }
